2b998ad94afd89f698f4594e76b97a9590206239
Azureit <d.f.18th@gmail.com>
2024-10-20 07:43:24 +0900

    c

---
+++ src/app/810/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import {motion} from 'framer-motion'
+
+const Gatiiku = () => {
+    return (
+        <motion.div
+      className="box"
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+    />
+    )
+}
+
+export default Gatiiku

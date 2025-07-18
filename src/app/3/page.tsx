7594df907fd905075106ff19a8bd9378ec3e6eb8
Azureit <d.f.18th@gmail.com>
2025-07-18 18:20:00 +0900

    c

--- src/app/3/page.tsx
+++ src/app/3/page.tsx
@@ -110,8 +110,8 @@ const Main = () => {
                 <FaGithub size={24} />
               </div>
             </motion.a>
-            <motion.a href="https://discord.gg/XkwSarHyQm" className="fade-up grid place-items-center gap-3 row-start-5 border shadow transition-colors hover:border-[rgb(88,101,242)] rounded-lg row-span-3 col-span-4">
-              <div className="flex h-10 w-10 rounded-xl text-white bg-[rgb(88,101,242)]">
+            <motion.a href="https://discord.gg/XkwSarHyQm" className="fade-up grid gap-3 row-start-5 border shadow transition-colors hover:border-[rgb(88,101,242)] rounded-lg row-span-3 col-span-4">
+              <div className="flex h-10 w-10 items-center justify-center rounded-xl text-white bg-[rgb(88,101,242)]">
                 <FaDiscord size={24} />
               </div>
             </motion.a>
